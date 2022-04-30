44cb3aa8154d1af6ebed4139671cd0eaa3be401f
musekorea <musekorea@hanmail.net>
2022-04-30 13:40:49 +0800

    reset

--- schema/movies/movie.query.js
+++
@@ -1,8 +0,0 @@
-import client from "../../client.js";
-
-export default {
-	Query: {
-		movies: () => client.Movies.findMany(),
-		movie: (_, { id }) => client.movies.findUnique({ where: { id } }),
-	},
-};
