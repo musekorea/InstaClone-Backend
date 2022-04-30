b1f96733e387fdb5e21ccd280b389b41c9bcc011
musekorea <musekorea@hanmail.net>
2022-04-30 13:17:50 +0800

    add dotenv

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+import "dotenv/config";
 import { ApolloServer, gql } from "apollo-server";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 import schema from "./graphql_shcema";
+
+const PORT = process.env.PORT;
 
 const server = new ApolloServer({
 	schema,
 	plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
 });
 
-server.listen().then(() => {
-	console.log("Server is running on PORT 4000");
+server.listen(PORT).then(() => {
+	console.log(`Server is running on PORT ${PORT}`);
 });
