925d600e40be0057a99946da3836cfb5e0ce62fa
musekorea <musekorea@hanmail.net>
2022-05-10 00:32:57 +0800

    prisma and create account mutation

--- schema/users/users.mutation.js
+++ schema/users/users.mutation.js
@@ -1,1 +1,15 @@
-export default {};
+import client from "../../client";
+
+export default {
+	Mutation: {
+		createAccount: async (
+			_,
+			{ firstName, lastName, userName, email, password }
+		) => {
+			const existUser = await client.user.findFirst({
+				where: { OR: [{ userName }, { email }] },
+			});
+			console.log(existUser);
+		},
+	},
+};
