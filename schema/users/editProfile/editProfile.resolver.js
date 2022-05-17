b383c875e75b6b156f8b8e0733328c164a28902f
musekorea <musekorea@hanmail.net>
2022-05-17 18:14:34 +0800

    edit profile

--- schema/users/editProfile/editProfile.resolver.js
+++ schema/users/editProfile/editProfile.resolver.js
@@ -1,7 +1,40 @@
+import client from "../../../client";
+import bcrypt from "bcrypt";
+
 export default {
 	Mutation: {
-		editProfile: () => {
-			console.log("test");
+		editProfile: async (
+			_,
+			{ firstName, lastName, userName, email, password }
+		) => {
+			try {
+				let hashedPassword = null;
+				if (password) {
+					hashedPassword = await bcrypt.hash(password, 10);
+				}
+				const updateUser = await client.user.update({
+					where: { id: 1 },
+					data: {
+						firstName,
+						lastName,
+						userName,
+						email,
+						password: hashedPassword ? hashedPassword : password,
+					},
+				});
+				if (updateUser) {
+					return {
+						success: true,
+					};
+				} else {
+					return {
+						success: false,
+						error: "Sorry, could not update profile",
+					};
+				}
+			} catch (error) {
+				console.log(error);
+			}
 		},
 	},
 };
