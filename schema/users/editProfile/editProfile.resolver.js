1d10e10e47483f5addfeded9c78537a9a8a865cd
musekorea <musekorea@hanmail.net>
2022-05-18 16:23:51 +0800

    authentication 1

--- schema/users/editProfile/editProfile.resolver.js
+++ schema/users/editProfile/editProfile.resolver.js
@@ -1,19 +1,22 @@
 import client from "../../../client";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export default {
 	Mutation: {
 		editProfile: async (
 			_,
-			{ firstName, lastName, userName, email, password }
+			{ firstName, lastName, userName, email, password, token }
 		) => {
 			try {
+				const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET_KEY);
+				console.log(decodedToken);
 				let hashedPassword = null;
 				if (password) {
 					hashedPassword = await bcrypt.hash(password, 10);
 				}
 				const updateUser = await client.user.update({
-					where: { id: 1 },
+					where: { id: decodedToken.id },
 					data: {
 						firstName,
 						lastName,
